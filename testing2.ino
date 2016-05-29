8da93aac8b9e158186b17a0a0461b5a6d4d9d4e8
Jasmine OuO <icecreamdreaming@gmail.com>
2016-05-29 14:02:55 -0400

    It WORKSSSS
    Both sets of traffic lights are timed and work. No arrays yet.

--- testing2.ino
+++ testing2.ino
@@ -11,15 +11,9 @@ byte yellowPinState = LOW;
 byte greenPinState = LOW; 
 byte red2PinState = LOW; 
 byte yellow2PinState = LOW; 
-byte green2PinState = LOW;  
+byte green2PinState = HIGH;  
 
 long currentMillis = 0;    // stores the value of millis() in each iteration of loop()
-long redPinMillis = 0;
-long yellowPinMillis = 0;
-long greenPinMillis = 0;
-long red2PinMillis = 0;
-long yellow2PinMillis = 0;
-long green2PinMillis = 0;
 
 long previousRedMillis=0;
 long previousYellowMillis=0;
@@ -28,6 +22,7 @@ long previousRed2Millis=0;
 long previousYellow2Millis=0;
 long previousGreen2Millis=0;
 bool first=false;
+bool first2=false;
 
 void setup() {
   pinMode(redPin, OUTPUT);
@@ -37,16 +32,29 @@ void setup() {
   pinMode(yellowPin2, OUTPUT);
   pinMode(greenPin2, OUTPUT);
 }
+
 void loop() {
    currentMillis = millis();
-   updateState(redPin,redPinState,previousRedMillis,4000,6000);
-   if (first==false)
-   updateState(greenPin,greenPinState,previousGreenMillis,4000,4000);
-   else
-   updateState(greenPin,greenPinState,previousGreenMillis,4000,6000);
-   updateState(yellowPin,yellowPinState,previousYellowMillis,2000,8000);
-   switchLeds();
    
+   updateState(redPin,redPinState,previousRedMillis,4000,4000);
+   
+   if (first==false)
+   updateState(greenPin,greenPinState,previousGreenMillis,3000,4000);
+   else
+   updateState(greenPin,greenPinState,previousGreenMillis,3000,5000);
+   
+   updateState(yellowPin,yellowPinState,previousYellowMillis,1000,7000);
+   
+   updateState(redPin2,red2PinState,previousRed2Millis,4000,4000);
+   
+   updateState(greenPin2,green2PinState,previousGreen2Millis,3000,5000);
+   
+   if (first2==false)
+   updateState(yellowPin2,yellow2PinState,previousYellow2Millis,1000,3000);
+   else
+   updateState(yellowPin2,yellow2PinState,previousYellow2Millis,1000,7000);
+   
+   switchLeds();
 }
 
 void updateState(int pin, byte &state, long &previousMillis, int intervalOn, int intervalOff){
@@ -55,6 +63,8 @@ void updateState(int pin, byte &state, long &previousMillis, int intervalOn, int
       state=HIGH;
       if (pin==8)
       first=true;
+      if (pin==6)
+      first2=true;
       previousMillis += intervalOff;
     }
   }
@@ -67,8 +77,10 @@ void updateState(int pin, byte &state, long &previousMillis, int intervalOn, int
 }
 
 void switchLeds() {
-     // this is the code that actually switches the LEDs on and off
  digitalWrite(redPin, redPinState);
  digitalWrite(greenPin, greenPinState);
  digitalWrite(yellowPin, yellowPinState);
+ digitalWrite(redPin2, red2PinState);
+ digitalWrite(greenPin2, green2PinState);
+ digitalWrite(yellowPin2, yellow2PinState);
 }
