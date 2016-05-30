7897b4f5890da26f2fce71124f8e95ad7c139c9f
Jasmine OuO <icecreamdreaming@gmail.com>
2016-05-29 21:28:03 -0400

    Traffic light with button
    button only pauses system for 8 seconds then continues on like nothing
    happened. no array.

---
+++ test4.ino
@@ -0,0 +1,126 @@
+int redPin = 10;
+int yellowPin = 9;
+int greenPin = 8;
+int redPin2 = 7;
+int yellowPin2 = 6;
+int greenPin2 = 5;
+int testPin=0;
+
+int buttonPin=1;
+
+byte redPinState = HIGH; 
+byte yellowPinState = LOW; 
+byte greenPinState = LOW; 
+byte red2PinState = LOW; 
+byte yellow2PinState = LOW; 
+byte green2PinState = HIGH; 
+int testState=LOW;
+ 
+int buttonState=0;
+
+long currentMillis = 0;    // stores the value of millis() in each iteration of loop()
+
+long previousRedMillis=0;
+long previousYellowMillis=0;
+long previousGreenMillis=0;
+long previousRed2Millis=0;
+long previousYellow2Millis=0;
+long previousGreen2Millis=0;
+
+long previousButtonMillis=0;
+
+bool first=false;
+bool first2=false;
+bool first3=false;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(redPin2, OUTPUT);
+  pinMode(yellowPin2, OUTPUT);
+  pinMode(greenPin2, OUTPUT);
+  pinMode(testPin,OUTPUT);
+  pinMode(buttonPin,INPUT);
+  buttonState=HIGH;
+}
+
+void loop() {
+   currentMillis = millis();
+   //buttonState=digitalRead(buttonPin);
+   readButton();
+   if (buttonState==LOW&&currentMillis-previousButtonMillis<8000)//button pressed
+  {
+   testState=HIGH;
+//   updateState(redPin,redPinState,previousRedMillis,2000,6000);
+//   updateState(greenPin,greenPinState,previousGreenMillis,5000,2000);
+//   updateState(yellowPin,yellowPinState,previousYellowMillis,1000,7000);
+//   updateState(redPin2,red2PinState,previousRed2Millis,6000,2000);
+//   updateState(greenPin2,green2PinState,previousGreen2Millis,1000,7000);
+//   if (first3==false)
+//   updateState(yellowPin2,yellow2PinState,previousYellow2Millis,1000,1000);
+//   else
+//   updateState(yellowPin2,yellow2PinState,previousYellow2Millis,1000,5000);
+   }
+   else//button state not pressed so HIGH
+   {
+   testState=LOW;
+   updateState(redPin,redPinState,previousRedMillis,4000,4000);
+   if (first==false)
+   updateState(greenPin,greenPinState,previousGreenMillis,3000,4000);
+   else
+   updateState(greenPin,greenPinState,previousGreenMillis,3000,5000);
+   updateState(yellowPin,yellowPinState,previousYellowMillis,1000,7000);
+   updateState(redPin2,red2PinState,previousRed2Millis,4000,4000);
+   updateState(greenPin2,green2PinState,previousGreen2Millis,3000,5000);
+   if (first2==false)
+   updateState(yellowPin2,yellow2PinState,previousYellow2Millis,1000,3000);
+   else
+   updateState(yellowPin2,yellow2PinState,previousYellow2Millis,1000,7000);
+   }
+   switchLeds();
+}
+
+void updateState(int pin, byte &state, long &previousMillis, int intervalOn, int intervalOff){
+  if (state==LOW){
+    if (currentMillis - previousMillis >=intervalOff){
+      state=HIGH;
+      if (pin==8)
+      first=true;
+      if (pin==6)
+      {
+      first2=true;
+      if (buttonState==LOW)
+      first3=true;
+      }
+      previousMillis += intervalOff;
+    }
+  }
+  else{
+    if (currentMillis - previousMillis >= intervalOn) {
+      state = LOW;
+      previousMillis += intervalOn;
+    }
+  }
+}
+
+void switchLeds() {
+ digitalWrite(redPin, redPinState);
+ digitalWrite(greenPin, greenPinState);
+ digitalWrite(yellowPin, yellowPinState);
+ digitalWrite(redPin2, red2PinState);
+ digitalWrite(greenPin2, green2PinState);
+ digitalWrite(yellowPin2, yellow2PinState);
+ digitalWrite(testPin, testState);
+}
+
+void readButton() {
+   if (digitalRead(buttonPin) == LOW) {
+     buttonState = LOW; 
+     previousButtonMillis=currentMillis;
+   }
+   if (currentMillis-previousButtonMillis>=8000)
+   {
+   buttonState=HIGH;
+   }
+ }
